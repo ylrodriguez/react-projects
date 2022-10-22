a7e956a76f83507e92efdb792c37a590f741ec82
Yojhan Rodriguez <yojhanr95@gmail.com>
2022-10-21 21:11:40 -0500

    86

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({
+	people
+}) => {
+	return (
+		<>
+			{
+				// Iterate over people and prints info
+				people.map((person) => {
+					return (
+						<article key={person.id} className='person'>
+							<img src={person.image} alt="" />
+							<div>
+								<h4>{person.name}</h4>
+								<p>{person.age} years</p>
+							</div>
+						</article>
+					)
+				})
+			}
+		</>
+	);
 };
 
 export default List;
