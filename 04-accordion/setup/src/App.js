7e6e19e4b88686a22a73d6a14dd230c7b5c877d4
Yojhan Rodriguez <yojhanr95@gmail.com>
2022-10-25 15:31:08 -0500

    4

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+	return (
+		<main>
+			<div className='container'>
+				<h3>Questions</h3>
+				<section className="info">
+					{
+						data.map(question => {
+							return <SingleQuestion key={question.id} question={question}></SingleQuestion>
+						})
+					}
+				</section>
+			</div>
+		</main>
+	)
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ question }) => {
+	const { id, title, info } = question;
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<article className="question">
+			<header>
+				<h4>{title}</h4>
+				<button className="btn" onClick={() => setIsOpen(!isOpen)}>
+					{isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+				</button>
+			</header>
+			{isOpen && <p>{info}</p>}
+		</article>
+	)
 };
 
 export default Question;
