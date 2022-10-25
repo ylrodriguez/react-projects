4182ff0bbed50e7c1c6ae3116e43008d670d3655
Yojhan Rodriguez <yojhanr95@gmail.com>
2022-10-25 15:23:11 -0500

    .

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Review from './Review';
+import reviewsData from './data';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+
+	const [reviews, setReviews] = useState([]);
+	const [selectedReview, setSelectedReview] = useState();
+	const [selectedIndex, setSelectedIndex] = useState();
+
+	const fetchReviews = () => {
+		setReviews(reviewsData);
+		setSelectedIndex(0);
+	};
+
+	function randomExcluded(start, end, excluded) {
+		var n = excluded
+		while (n == excluded)
+			n = Math.floor((Math.random() * (end - start + 1) + start));
+		return n;
+	}
+
+	useEffect(() => {
+		fetchReviews();
+	}, []);
+
+	useEffect(() => {
+		setSelectedReview(reviews[selectedIndex]);
+	}, [selectedIndex])
+
+	const onChangeReview = (action) => {
+		debugger
+		let index = 0;
+		switch (action) {
+			case 'next':
+				index = selectedIndex == reviews.length - 1 ? 0 : selectedIndex + 1;
+				break;
+			case 'prev':
+				index = selectedIndex <= 0 ? reviews.length - 1 : selectedIndex - 1;
+				break;
+			case 'random':
+				index = randomExcluded(0, reviews.length - 1, selectedIndex)
+				break;
+		}
+		setSelectedIndex(index);
+	}
+
+	return (
+		<main>
+			<section className='container'>
+				<div className='title'>
+					<h2>Our Reviews</h2>
+					<div className="underline"></div>
+				</div>
+				{selectedReview &&
+					<Review
+						review={selectedReview}
+						onChangeReview={onChangeReview}>
+					</Review>}
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
-import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = () => {
-  return <h2>review component</h2>;
+const Review = ({ review, onChangeReview }) => {
+	const { id, name, job, image, text } = review;
+	return (
+		<article className='review'>
+			<div className="img-container">
+				<img className='person-img' src={image} alt={review.name} />
+				<span className="quote-icon"><FaQuoteRight /></span>
+			</div>
+			<h4 className='author'>{name}</h4>
+			<p className="job">{job}</p>
+			<p className="info">{text}</p>
+			<div className="button-container">
+				<button className="prev-btn" onClick={() => onChangeReview('prev')}><FaChevronLeft /></button>
+				<button className="next-btn" onClick={() => onChangeReview('next')}><FaChevronRight /></button>
+			</div>
+			<button className="random-btn" onClick={() => onChangeReview('random')}>Surprise me</button>
+		</article>
+	)
 };
 
 export default Review;
